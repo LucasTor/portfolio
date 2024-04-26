3aadc7f497cb3234267b4ff70f0398d81135162a
Lucas Torresan <lucastorresan@hotmail.com>
2024-04-26 14:24:30 -0300

    Updates

---
+++ precache-manifest.efe03222b97b999e18ea7f9f09902e29.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b56a227a04f5022d7e315d03e29a81fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d4a5af74401594bfd006",
+    "url": "/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "ceb82bc7d0595376a75e",
+    "url": "/static/css/main.98e576d8.chunk.css"
+  },
+  {
+    "revision": "d4a5af74401594bfd006",
+    "url": "/static/js/2.001f75a1.chunk.js"
+  },
+  {
+    "revision": "484b3a90b75ec38dbb4ca2cd8fcfc3fd",
+    "url": "/static/js/2.001f75a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ceb82bc7d0595376a75e",
+    "url": "/static/js/main.1e44bfff.chunk.js"
+  },
+  {
+    "revision": "c044c296fef9e582399f",
+    "url": "/static/js/runtime-main.4070baf2.js"
+  },
+  {
+    "revision": "7c4aed56245ae966a682cb5ab46dee43",
+    "url": "/static/media/profile.7c4aed56.png"
+  }
+]);
